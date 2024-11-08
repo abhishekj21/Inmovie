43ac34f8d15da4f4497ceb3875b8547431c7952d
Abhishek Singh <singhabhishekj21@gmail.com>
2024-11-08 13:42:11 +0000

    final version 1 completed applied auth feature and responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import search_icon from "../../assets/search_icon.svg";
 import bell_icon from "../../assets/bell_icon.svg";
 import profile_img from "../../assets/profile_img.png";
 import caret_icon from "../../assets/caret_icon.svg";
+import { logout } from "../../../Firebase";
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 80) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
   return (
-    <div className="navbar">
+    <div className={`navbar ${isScrolled ? "scrolled" : ""}`}>
       <div className="navbar-left">
         <img src={logo} alt="" />
         <ul>
@@ -22,13 +47,21 @@ const Navbar = () => {
       <div className="navbar-right">
         <img src={search_icon} alt="" className="icons" />
         <p>Children</p>
-        <img src={bell_icon} alt="" className="icons" />
-        <div className="navbar-profile">
+        <img src={bell_icon} alt="Notifications" className="icons" />
+        <div className="navbar-profile" onClick={toggleDropdown}>
           <img src={profile_img} alt="" className="profile" />
           <img src={caret_icon} alt="" />
-          <div className="dropdown">
-            <p>Sign Out of Inmovie</p>
-          </div>
+          {isDropdownOpen && (
+            <div className="dropdown">
+              <p
+                onClick={() => {
+                  logout();
+                }}
+              >
+                Sign Out of Inmovie
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </div>
